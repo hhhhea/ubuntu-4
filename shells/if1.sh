d0e2146a2899ad78ce3b76ff532d9da27c3b465f
haedal <haechang977@gmail.com>
2022-11-02 18:19:05 +0900

    10/26.

--- shells/if1.sh
+++ shells/if1.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-echo "file name : $0"
-echo "Parameter : $#"
-echo "All Parameters : $@" 
+echo "file name : $0" 
 if [ "$1" = ok ]; then
 	echo "good~!!"
 else
 	echo "bad~!!"
-fi
+fi 
--- shells/while2.sh
+++ shells/while2.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
-row="$1"
 
-if [ $# -eq 0 ]; then
-	echo "this program require one parameter"
-elif [ $row -lt 10 -a $row -ge 0 ]; then
+row="$1"
 while [[ 10 -gt $row ]]; do
 	echo "$row"
 	row=`echo "$row"+1|bc`
 done
-else
-	echo "1~10까지 숫자입력"
-fi
